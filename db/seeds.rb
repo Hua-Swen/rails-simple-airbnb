7dac4c88a2105d43a1b63e238a22e8f53b5b22cb
tan hua swen <tan.huaswen@gmail.com>
2020-03-11 14:38:59 +0800

    installed bootstrap and simple_form, used scaffolding to create the simple page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "dropping all database"
+Flat.destroy_all
+
+puts "generating 5 flats"
+
+5.times do
+  Flat.create!(
+    name: Faker::Address.street_name,
+    address: Faker::Address.full_address,
+    description: Faker::Restaurant.review,
+    price_per_night: rand(50..150),
+    number_of_guests: rand(5..10),
+    image_url: "https://source.unsplash.com/random"
+  )
+end
+
+puts "Done!"
